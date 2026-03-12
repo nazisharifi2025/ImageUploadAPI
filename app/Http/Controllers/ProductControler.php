045e1c8c712086bb0440426671d8a914b9f96630
nazi sharifi <banonazisharifi@gmail.com>
2026-03-12 17:43:27 +0430

    new chenges

--- app/Http/Controllers/ProductControler.php
+++ app/Http/Controllers/ProductControler.php
@@ -19,7 +19,7 @@ class ProductControler extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+       $validatedData = $request->validate([
             'name' => 'required|string',
             'price' => 'required|numeric',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
